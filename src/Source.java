90b79967bd20496e99d51c7085c33ab07bd12961
Jakub Baran <b.kuba.13@gmail.com>
2021-04-26 21:38:03 +0200

    First Class

--- src/Source.java
+++ src/Source.java
@@ -1,5 +1,24 @@
 public class Source{
-    public static void main(String... args) {
-        System.out.println("NewLine");
+    public static void main(String[] args) {
+        System.out.println("Chess");
+    }
+}
+
+abstract class Piece{
+    int id;
+    boolean isAlive;
+    public int posX;
+    int posY;
+}
+
+class King extends Piece{
+    King(){
+        this.posX = 1;
+        this.posY = 1;
+        this.isAlive = true;
+        this.id = 1;
+    }
+    public void move(){
+
     }
 }
